23b3ac9be3b80df83e840fc20863831c34e99455
Maneesha <38124106+mani566@users.noreply.github.com>
2022-10-10 13:55:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('omega 3 and Blueberry Oatmeal')
-steramlit.text('kale,Spinach & rocket smoothi')
-steramlit.text('Hard-Boiled Free Range Egg')
+streamlit.text('kale,Spinach & rocket smoothi')
+streamlit.text('Hard-Boiled Free Range Egg')
